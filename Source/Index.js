764ee3876d6182ce393985908ae9cd780e5ea16e
hunter <email.hunter.jager@gmail.com>
2020-08-09 11:22:03 -0500

    minimize skript

--- Source/Index.js
+++ Source/Index.js
@@ -13,28 +13,20 @@ const Server = Express();
 
 
 Server.get('/Home.css', (req, res) => {
-
     res.sendFile(Path.join(__dirname + '/Static/Home.css'))
-
 })
 
 
 Server.get('/header.svg', (req, res) => {
-
     res.sendFile(Path.join(__dirname + '/Static/header.svg'))
-
 })
 
 
 Server.get('/', (req, res) => {
-
     res.sendFile(Path.join(__dirname + '/Views/Index.html'))
-
 })
 
 
 Server.listen(8080, () => {
-
     console.log('Application Deployed!')
-
 })
