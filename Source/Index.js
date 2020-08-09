c37b9e256fcdf70502183d0db6a756dae5af9506
hunter <email.hunter.jager@gmail.com>
2020-08-09 09:26:06 -0500

    Basic Setup of Express and File Structure Cont.

---
+++ Source/Index.js
@@ -0,0 +1,33 @@
+
+
+/*
+
+    Index.js
+
+*/
+
+
+const Express = require('express');
+const Path = require('path');
+const Server = Express();
+
+
+Server.get('/Home.css', (req, res) => {
+
+    res.sendFile(Path.join(__dirname + '/Static/Home.css'))
+
+})
+
+
+Server.get('/', (req, res) => {
+
+    res.sendFile(Path.join(__dirname + '/Views/Index.html'))
+
+})
+
+
+Server.listen(8080, () => {
+
+    console.log('Application Deployed!')
+
+})
