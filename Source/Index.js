b2f608ca5e657fe9a7fa28285240eb355cf1ad93
hunter <email.hunter.jager@gmail.com>
2020-08-10 17:37:53 -0500

    Revamped the Project section and fixed a few bugs.

--- Source/Index.js
+++ Source/Index.js
@@ -22,11 +22,16 @@ Server.get('/header.svg', (req, res) => {
 })
 
 
+Server.get('/portfolio.png', (req, res) => {
+    res.sendFile(Path.join(__dirname + '/Static/unknown.png'))
+})
+
+
 Server.get('/', (req, res) => {
     res.sendFile(Path.join(__dirname + '/Views/Index.html'))
 })
 
 
 Server.listen(8080, () => {
-    console.log('Application Deployed! Goto http://localhost:8080/ to visit my portfolio.')
+    console.log('Application Deployed! Goto http://localhost:8080/')
 })
